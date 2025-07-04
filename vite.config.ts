286e3ee529ac7426cfa6ff1abfd25de8f0fa51ac
Thomas Robin <37398795+totorobin@users.noreply.github.com>
2025-07-04 17:59:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    allowedHosts: true
-  },
-  build: {
-     base: '/kitsune-legacy/'
-  }
+  base: './'
 })
