25cd68a966cf9ec95986ecf3208ad33c17727323
totorobin <totorobin@gmail.com>
2025-07-05 23:26:15 +0200

    Remove `dist` folder from repository and cleanup associated build artifacts and add PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      } ,
+      manifest: {
+        name: 'Kitsune Legacy',
+        short_name: 'Kitsune',
+        description: 'Kitsune - Le jeu des nombres'
+      }
+    })
+  ],
   base: './'
 })
